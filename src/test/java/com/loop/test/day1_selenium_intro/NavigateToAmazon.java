fb702a92f517855a4cc943ff487e0808d2077944
product077 <products0707@gmail.com>
2023-11-18 22:20:02 -0500

    My first Selenium

---
+++ src/test/java/com/loop/test/day1_selenium_intro/NavigateToAmazon.java
@@ -0,0 +1,33 @@
+package com.loop.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class
+NavigateToAmazon {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+
+
+
+
+
+        /*
+    navigate to amazon
+    maximize
+    navigate back
+    navigate forward
+    refresh
+     */
+
+    }
+}
